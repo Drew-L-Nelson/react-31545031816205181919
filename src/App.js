df52c2757c1de50aefd847f66730b4aff6a601a6
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-05-22 11:42:18 -0700

    MUI Added, fixing routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./style.css";
+import Header from './components/Header'
+import { Routes, Route, Switch } from 'react-router-dom';
 
-export default function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+      <Header />
   );
-}
+};
+
+export default App;
+
+
+{/* <Route exact path="/" component={AboutPage} />
+<Route path="/characters" component={CharactersPage} />
+<Route path="/book" component={BookPage} />
+<Route path="/newsletter" component={NewsletterPage} />
+<Route path="/contact" component={ContactPage} />
+<Route path="/our-mission" component={OurMissionPage} />
+<Route path="/shop" component={ShopPage} /> */}
