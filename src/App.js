a8e6496b736ffd657842c074578f75a1b6084e5b
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-06-12 11:02:09 -0700

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 import Book from './pages/Book';
 import Characters from './pages/Characters';
 import Newsletter from './pages/Newsletter';
-import OurMission from './pages/OurMission';
+import About from './pages/About';
 import Shop from './pages/Shop';
 import { Routes, Route } from 'react-router-dom';
 
@@ -20,7 +20,7 @@ const App = () => {
         <Route path="/Book" element={<Book/>} />
         <Route path="/Characters" element={<Characters/>} />
         <Route path="/Newsletter" element={<Newsletter/>} />
-        <Route path="/Our%20Mission" element={<OurMission/>} />
+        <Route path="/About" element={<About/>} />
         <Route path="/Shop" element={<Shop/>} />
       </Routes>
     </div>
