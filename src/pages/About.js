75ebfd08c0d34c67909cd0eabe60828de8388a03
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-06-13 12:30:21 -0700

    fixed more bugs

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/About.css'
 import { getAboutImageUrl } from '../firebase-config.js'
-import { useParallax } from 'react-scroll-parallax';
+import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax';
 
 const About = () => {
   const [imageUrl, setImageUrl] = useState(null);
@@ -18,8 +18,26 @@ const About = () => {
 
 
   return (
-    <div>
-      {imageUrl && <img src={imageUrl} className='main-about-image' alt="AboutImage" />}
+    <div className="image-overlay">
+      
+      <ParallaxBanner
+        style={{ aspectRatio: '2 / 1' }}
+        layers={[
+          { image: imageUrl, speed: -20 },
+          { 
+            speed: -10,
+            children: (
+              <div className="headline">
+                <h1>My Headline</h1>
+              </div>
+            ),
+          },
+        ]}
+        className="aspect-[2/1]"
+      >
+
+      </ParallaxBanner>
+      
     </div>
   )
 }
