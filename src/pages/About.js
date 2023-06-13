21c14de36ea6490e1a40a05959bb8e4bc0423fcf
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-06-13 14:25:56 -0700

    2 files modified

--- src/pages/About.js
+++ src/pages/About.js
@@ -32,10 +32,29 @@ const About = () => {
               </div>
             ),
           },
+          { 
+            speed: -20,
+            children: (
+              <div>
+                <p>
+                Welcome to the fascinating universe of Code Crafters! This is where young minds are kindled by the thrill of adventure, the wonder of technology, and the sheer joy of storytelling. Our unique tales, set in the vibrant, digital landscape of Pixela, blend the magic of fiction with the exciting world of coding, transforming how middle-grade readers perceive and engage with technology.
+
+                Our heroes, Vara and Kai, journey through the dazzling expanse of Pixela using their unique abilities, to tackle challenges and solve problems. Each thrilling adventure is intricately woven with coding concepts, making learning an inherent part of their epic quests.
+
+                At Code Crafters, our vision is to ignite a love for coding among young readers. Through our stories, we aim to demystify coding, presenting it not just as a useful skill, but as a tool for creative expression and problem-solving. As our characters tackle various challenges, readers are inspired to think logically, embrace innovation, and approach problems with a coder's mindset.
+
+                Our narratives are not just about coding, though. They also highlight the power of curiosity, resilience, and teamwork. Each story is a fun-filled adventure with engaging mysteries, formidable foes, and a hearty dose of humor. While our young heroes explore the digital world, they also learn valuable lessons about friendship, bravery, and the joy of discovery.
+
+                Code Crafters is for every young reader ready for a grand adventure, every parent seeking meaningful yet entertaining content, and every educator on the hunt for dynamic, enriching material. We're excited to take you on a journey that's as much about learning to code as it is about enjoying a great story.
+                Join us at Code Crafters - let's inspire, create, and craft incredible coding adventures together!
+                </p>
+              </div>
+            ),
+          },
         ]}
         className="aspect-[2/1]"
       >
-
+        
       </ParallaxBanner>
       
     </div>
